20c5dd65eb65b1e5cb2d47dc2da3822adec8225a
MAli Garanli <muhammed_ali.garanli@smaik.th-koeln.de>
2025-09-18 07:15:33 +0200

    Postgres

--- src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/Actor/OutboxRepository.java
+++ src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/Actor/OutboxRepository.java
@@ -1,10 +1,12 @@
 package de.thk.gm.gdw.fitamcampus_muhammedali_garanli.Actor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OutboxRepository extends JpaRepository<Outbox, Long> {
-    List<Outbox> findByUsername(String username);
+    //List<Outbox> findByUsername(String username);
     List<Outbox> findAllByActivityJsonContaining(String username);
 }
