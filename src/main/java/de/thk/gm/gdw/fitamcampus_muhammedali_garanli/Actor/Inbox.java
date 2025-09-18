af01c6a3005da51074b86d3df889f64bd6c8da08
MAli Garanli <muhammed_ali.garanli@smaik.th-koeln.de>
2025-09-18 03:41:36 +0200

    Controller

--- src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/Actor/Inbox.java
+++ src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/Actor/Inbox.java
@@ -1,12 +1,14 @@
 package de.thk.gm.gdw.fitamcampus_muhammedali_garanli.Actor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
 
+@Controller
 public class Inbox {
 
     @PostMapping(value = "/users/{username}/inbox", consumes = "application/activity+json")
--- src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/Actor/Outbox.java
+++ src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/Actor/Outbox.java
@@ -1,5 +1,6 @@
 package de.thk.gm.gdw.fitamcampus_muhammedali_garanli.Actor;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -7,6 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+@Controller
 public class Outbox {
 
     @GetMapping(value = "/users/{username}/outbox", produces = "application/activity+json")
