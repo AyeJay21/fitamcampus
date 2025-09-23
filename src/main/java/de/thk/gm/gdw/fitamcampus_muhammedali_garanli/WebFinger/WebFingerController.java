8c2ff66acf685b08c7252328121673625793d5e8
MAli Garanli <muhammed_ali.garanli@smaik.th-koeln.de>
2025-09-23 14:07:34 +0200

    Webfinger

--- src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/WebFinger/WebFingerController.java
+++ src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/WebFinger/WebFingerController.java
@@ -1,5 +1,6 @@
 package de.thk.gm.gdw.fitamcampus_muhammedali_garanli.WebFinger;
 
+import de.thk.gm.gdw.fitamcampus_muhammedali_garanli.actor.ActorRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,24 +15,38 @@ import java.util.Map;
 @RestController
 public class WebFingerController {
 
-    @GetMapping(value = ".well-known/webfinger", produces = "application/activity+json")
-    public ResponseEntity<?> webfinger(@RequestParam String resource){
-        if (!resource.equals("acct:ayejay@alluneedspot.com")) {
-            return ResponseEntity.status(404).body(Map.of("error", "User not found"));
+    private final ActorRepository actorRepository;
+
+    public WebFingerController(ActorRepository actorRepository) {
+        this.actorRepository = actorRepository;
+    }
+
+    @GetMapping(value = "/.well-known/webfinger", produces = "application/jrd+json")
+    public ResponseEntity<?> webfinger(@RequestParam String resource) {
+        if (!resource.startsWith("acct:")) {
+            return ResponseEntity.badRequest().body(Map.of("error", "Invalid resource format"));
         }
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("subject", "acct:ayejay@alluneedspot.com");
+        // resource sieht aus wie: acct:ayejay@alluneedspot.com
+        String acct = resource.substring(5); // "ayejay@alluneedspot.com"
+        String username = acct.split("@")[0]; // "ayejay"
 
-        List<Map<String, Object>> links = new ArrayList<>();
-        Map<String, Object> self = new HashMap<>();
-        self.put("rel", "self");
-        self.put("type", "application/activity+json");
-        self.put("href","https://activitypub.alluneedspot.com/users/ayejay");
-        links.add(self);
+        return actorRepository.findByUsername(username)
+                .map(actor -> {
+                    Map<String, Object> response = new HashMap<>();
+                    response.put("subject", resource);
 
-        response.put("links", links);
+                    List<Map<String, Object>> links = new ArrayList<>();
+                    Map<String, Object> self = new HashMap<>();
+                    self.put("rel", "self");
+                    self.put("type", "application/activity+json");
+                    self.put("href", "https://activitypub.alluneedspot.com/users/" + username);
+                    links.add(self);
 
-        return ResponseEntity.ok(response);
+                    response.put("links", links);
+
+                    return ResponseEntity.ok(response);
+                })
+                .orElseGet(() -> ResponseEntity.status(404).body(Map.of("error", "User not found")));
     }
 }
