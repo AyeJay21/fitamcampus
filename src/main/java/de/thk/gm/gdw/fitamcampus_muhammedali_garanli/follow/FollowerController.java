62d97b936608f1d0c014f7b83110202f6f46c3da
MAli Garanli <muhammed_ali.garanli@smaik.th-koeln.de>
2025-10-13 05:26:08 +0200

    Follow Request accept

--- src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/follow/FollowerController.java
+++ src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/follow/FollowerController.java
@@ -1,5 +1,8 @@
 package de.thk.gm.gdw.fitamcampus_muhammedali_garanli.follow;
 
+import de.thk.gm.gdw.fitamcampus_muhammedali_garanli.activityPub.ActivityPubDeliveryService;
+import de.thk.gm.gdw.fitamcampus_muhammedali_garanli.activityPub.RemoteActorService;
+import de.thk.gm.gdw.fitamcampus_muhammedali_garanli.actor.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +16,16 @@ public class FollowerController {
     @Autowired
     private FollowerRepository followerRepository;
 
-    // Follower-Request annehmen
+        @Autowired
+        private ActorService actorService;
+
+        @Autowired
+        private RemoteActorService remoteActorService;
+
+        @Autowired
+        private ActivityPubDeliveryService deliveryService;
+
+    // Follower-Request annehmen und Accept-Activity senden
     @PostMapping("/accept")
     public ResponseEntity<?> acceptFollower(@PathVariable String username, @RequestBody Map<String, String> body) {
         String followerUrl = body.get("followerUrl");
@@ -24,6 +36,36 @@ public class FollowerController {
         follower.setUsername(username);
         follower.setFollowerUrl(followerUrl);
         followerRepository.save(follower);
+
+        try {
+            // Lokalen Actor holen
+            var me = actorService.getActorByUsername(username);
+            String actorId = "https://activitypub.alluneedspot.com/users/" + me.getUsername();
+            String privateKey = me.getPrivateKeyPem();
+
+            // Remote Inbox und Actor URL auflösen
+            String targetInbox = remoteActorService.resolveActorInbox(followerUrl);
+            String targetActorUrl = remoteActorService.resolveActorUrl(followerUrl);
+
+            // Accept-Aktivität bauen
+            java.util.Map<String, Object> accept = new java.util.HashMap<>();
+            accept.put("@context", "https://www.w3.org/ns/activitystreams");
+            accept.put("id", actorId + "/activities/accept-" + java.util.UUID.randomUUID());
+            accept.put("type", "Accept");
+            accept.put("actor", actorId);
+            // Das Objekt ist die ursprüngliche Follow-Aktivität
+            java.util.Map<String, Object> followObj = new java.util.HashMap<>();
+            followObj.put("type", "Follow");
+            followObj.put("actor", targetActorUrl);
+            followObj.put("object", actorId);
+            accept.put("object", followObj);
+
+            // Senden
+            deliveryService.sendToInbox(targetInbox, accept, actorId, privateKey);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(Map.of("error", "Follower accepted, but failed to federate: " + e.getMessage()));
+        }
+
         return ResponseEntity.ok(Map.of("success", true, "followerUrl", followerUrl));
     }
 
