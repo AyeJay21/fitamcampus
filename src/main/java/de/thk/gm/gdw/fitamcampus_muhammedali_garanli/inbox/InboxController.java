0a8309881129cbfb352312a1e32f8edef724ab8c
MAli Garanli <muhammed_ali.garanli@smaik.th-koeln.de>
2025-10-15 02:12:09 +0200

    Inbox pls

--- src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/inbox/InboxController.java
+++ src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/inbox/InboxController.java
@@ -1,5 +1,6 @@
 package de.thk.gm.gdw.fitamcampus_muhammedali_garanli.inbox;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,25 +28,33 @@ public class InboxController {
     public Map<String, Object> addToInbox(@PathVariable String username, @RequestBody Map<String, Object> activity) throws Exception {
         System.out.println("Empfangene Activity: " + activity);
         Inbox inbox = new Inbox();
+        ObjectMapper mapper = new ObjectMapper();
+
         inbox.setActivity(activity);
         inbox.setUsername(username);
-        inbox.setType((String) activity.get("type"));
+        String type = (String) activity.get("type");
+        inbox.setType(type);
         inbox.setActor((String) activity.get("actor"));
+        
         inbox.setObjectData(activity.get("object").toString());
-        if ("Create".equals(activity.get("type")) && activity.get("object") instanceof Map) {
-            Map<String, Object> note = (Map<String, Object>) activity.get("object");
-            String content = (String) note.get("content");
-            String inReplyTo = (String) note.get("inReplyTo");
-            String noteActor = (String) note.get("attributedTo");
+        if (activity.get("object") instanceof Map<?,?> objectMap) {
+            inbox.setObjectData(mapper.writeValueAsString(mapper.writeValueAsString(objectMap)));
+            inbox.setObjectType((String) objectMap.get("type"));
+            inbox.setContent((String) objectMap.get("content"));
+            inbox.setAttributedTo((String) objectMap.get("attributedTo"));
+            inbox.setPublished((String) objectMap.get("published"));
+            inbox.setTo((objectMap.get("to") != null) ? objectMap.get("to").toString() : null);
         }
 
         Inbox saved = inboxRepository.save(inbox);
-        
+
         return Map.of(
-            "success", true,
-            "message", "Activity received",
-            "activityType", saved.getType(),
-            "from", saved.getActor()
+                "success", true,
+                "message", "Activity received",
+                "activityType", type,
+                "from", inbox.getActor(),
+                "objectType", inbox.getObjectType(),
+                "content", inbox.getContent()
         );
     }
 }
