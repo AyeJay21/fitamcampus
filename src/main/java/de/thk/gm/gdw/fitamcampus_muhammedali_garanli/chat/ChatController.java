fc93075100e1a8cb3f576ecb838e62cccc7fb886
MAli Garanli <muhammed_ali.garanli@smaik.th-koeln.de>
2025-10-18 12:53:14 +0200

    follower

--- src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/chat/ChatController.java
+++ src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/chat/ChatController.java
@@ -1,15 +1,28 @@
 package de.thk.gm.gdw.fitamcampus_muhammedali_garanli.chat;
 
+import de.thk.gm.gdw.fitamcampus_muhammedali_garanli.message.Message;
+import de.thk.gm.gdw.fitamcampus_muhammedali_garanli.message.MessageRepository;
 import de.thk.gm.gdw.fitamcampus_muhammedali_garanli.navigation.LoginController;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class ChatController {
+
+    private final MessageRepository messageRepository;
+
+    public ChatController(MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
+    }
 
     @GetMapping("/users/chats")
     public String openChat(Model model, HttpSession session) throws Exception {
@@ -21,4 +34,19 @@ public class ChatController {
         return "chat";
     }
 
+    @GetMapping("/users/{username}/chats")
+    public String getChat(@PathVariable String username, @RequestBody String receiver, Model model){
+        List<Message> allMessages = messageRepository.findAll();
+        String userA = username;
+        String userB = receiver;
+
+        List<Message> chatMessages = allMessages.stream()
+                .filter(m -> (m.getSender().equals(userA) && m.getReciever().equals(userB)) ||
+                        (m.getSender().equals(userB) && m.getReciever().equals(userA)))
+                .sorted(Comparator.comparing(Message::getTimeStamp))
+                .collect(Collectors.toList());
+
+        model.addAttribute("chatMessages", chatMessages);
+        return "chat";
+    }
 }
