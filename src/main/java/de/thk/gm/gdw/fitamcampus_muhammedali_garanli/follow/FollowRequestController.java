3cd06cd657c6a067a960289fba4bca8847351cae
MAli Garanli <muhammed_ali.garanli@smaik.th-koeln.de>
2025-10-27 00:34:57 +0100

    FollowRequest bad debugginf skills :(

--- src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/follow/FollowRequestController.java
+++ src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/follow/FollowRequestController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -15,6 +17,7 @@ public class FollowRequestController {
     FollowerService followerService;
 
     @GetMapping("/users/{username}/followRequests")
+    @ResponseBody
     public List<FollowRequest> getFollowRequest(@PathVariable String username){
         return followerService.getFollowerRequest(username);
     }
