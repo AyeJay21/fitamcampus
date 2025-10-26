19db18f07a7f6e841398195eb081c86db80075ba
MAli Garanli <muhammed_ali.garanli@smaik.th-koeln.de>
2025-10-26 13:56:24 +0100

    FollowRequest Rest

---
+++ src/main/java/de/thk/gm/gdw/fitamcampus_muhammedali_garanli/follow/FollowRestRequestController.java
@@ -0,0 +1,29 @@
+package de.thk.gm.gdw.fitamcampus_muhammedali_garanli.follow;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class FollowRestRequestController {
+
+    @Autowired
+    FollowerService followerService;
+
+    @GetMapping("/users/{username}/followRequests")
+    public ResponseEntity<List<FollowRequest>> getFollowRequest(@PathVariable String username){
+        List<FollowRequest> requests = followerService.getFollowerRequest(username);
+        return ResponseEntity.ok(requests);
+    }
+
+    @GetMapping("/followerRequest/{username}")
+    public String redirectFolloweRequest(@PathVariable String username, Model model){
+        model.addAttribute("username", username);
+        return "followerRequest";
+    }
+}
